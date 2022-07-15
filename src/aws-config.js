61549397e88bb59984f0d8e8fd2b724a29240528
neel.k <neel.k@zymr.com>
2022-07-15 13:31:44 +0530

    New Commit

---
+++ src/aws-config.js
@@ -0,0 +1,12 @@
+const awsconfig = {
+  API: {
+    endpoints: [
+      {
+        name: "greetingsEndpoint",
+        endpoint: "https://xxocsru40f.execute-api.ap-south-1.amazonaws.com/dev/",
+      },
+    ],
+  },
+};
+
+export default awsconfig;
